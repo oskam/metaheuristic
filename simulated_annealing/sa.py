1fe09c6fb549deb431d9dcff6027eebc83429402
oskam <oska.magdalena@gmail.com>
2017-05-11 08:55:24 +0200

    kill handling

--- simulated_annealing/sa.py
+++ simulated_annealing/sa.py
@@ -1,14 +1,14 @@
 import sys
 import math
 import random
-# from random import shuffle
 from time import time
 
 start = time()
 in_data = sys.stdin.readlines()
 
 n = int(float(in_data[0]))
-data = [[float(l) for l in line.split()[1:]] for line in in_data[1:]]
+data = [[float(l) for l in line.split()[1:n]] for line in in_data[1:n]]
+max_time = float(in_data[n+1])
 
 distance = [[math.hypot(x2 - x1, y2 - y1) for (x2, y2) in data] for (x1, y1) in data]
 
@@ -26,6 +26,7 @@ for i in range(0, len(distance)):
 
 for i, _ in enumerate(distance_copy):
     distance_copy[i][i] = inf
+
 
 # returns index of minimum value in row `k` of `matrix`
 def index_min(matrix, k):
@@ -46,7 +47,7 @@ for i in range(1, len(current_route) - 1):
     current_node = current_route[i]
     distance_copy[int(current_node) - 1][0] = inf
 
-    #forbid returns in next step to nodes already visited
+    # forbid returns in next step to nodes already visited
     for determined_route_node in range(i + 1):
         distance_copy[int(current_node) - 1][current_route[determined_route_node] - 1] = inf
 
@@ -58,12 +59,6 @@ print(current_cost)
 best_route = list(current_route)
 best_cost = current_cost
 
-# neighbor_set = [(x, y) for x in range(1, len(current_route) - 3) for y in range(x + 1, len(current_route) - 2)]
-
-
-# neighbors_to_check = (math.sqrt(len(neighbor_set)))
-
-
 neighbor_route = [0] * (n + 1)
 next_cost = 0
 
@@ -74,7 +69,7 @@ iterations = int(n)
 t0 = 100
 t = t0
 end_factor = 0.00001
-t_alpha = 0.99995
+t_alpha = 0.99999
 reheat_factor = 0.1
 
 
@@ -82,16 +77,11 @@ def acceptance_probability(old_cost, new_cost, tm):
     a = (float((float(new_cost - old_cost)) / (float(tm))))
     return math.exp(-a)
 
-max_time = 0.08*n+10
+
 random.seed()
 
-# old_cost = best_cost
 
 while t > t0*end_factor:
-    # neighbor_set_copy = list(neighbor_set)
-    # random.shuffle(neighbor_set_copy)
-
-    # x, y = neighbor_set_copy.pop()
     x = random.randint(1, len(current_route) - 4)
     y = random.randint(x+1, len(current_route) - 3)
 
@@ -129,13 +119,13 @@ while t > t0*end_factor:
 
     t = t * t_alpha
 
-print("T = ", t)
+    if time() - start > max_time - 1:
+        break
 
-
-# print(time() - start)
-# end = time()
-# print(end - start)
 
 print(best_cost)
-# for k in best_route:
-#     sys.stderr.write(str(k) + " ")
+for k in best_route:
+    sys.stderr.write(str(k) + " ")
+
+# end = time()
+# print(end - start)
